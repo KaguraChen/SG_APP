d2011ecf14c05a2ec1140a8fb46ef58d6672bac6
KagruaChen <2684607398@qq.com>
2023-02-07 11:01:35 +0800

    CameraGetActivity

--- app/src/main/java/com/example/sg_app/CameraGetActivity.java
+++ app/src/main/java/com/example/sg_app/CameraGetActivity.java
@@ -1,14 +1,104 @@
 package com.example.sg_app;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCamera2View;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CameraGetActivity extends AppCompatActivity {
+
+public class CameraGetActivity extends CameraActivity {
+
+    private static final String TAG = "OpencvCam";
+
+    private JavaCamera2View javaCameraView;
+    private int cameraId = JavaCamera2View.CAMERA_ID_ANY;
+
+    private CameraBridgeViewBase.CvCameraViewListener2 cvCameraViewListener2 = new CameraBridgeViewBase.CvCameraViewListener2() {
+        @Override
+        public void onCameraViewStarted(int width, int height) {
+            Log.i(TAG, "onCameraViewStarted width=" + width + ", height=" + height);
+        }
+
+        @Override
+        public void onCameraViewStopped() {
+            Log.i(TAG, "onCameraViewStopped");
+        }
+
+        @Override
+        public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+            return inputFrame.rgba();
+        }
+    };
+
+    private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            Log.i(TAG, "onManagerConnected status=" + status + ", javaCameraView=" + javaCameraView);
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    if (javaCameraView != null) {
+                        javaCameraView.setCvCameraViewListener(cvCameraViewListener2);
+                        // 禁用帧率显示
+                        javaCameraView.disableFpsMeter();
+                        javaCameraView.enableView();
+                    }
+                }
+                break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    //复写父类的 getCameraViewList 方法，把 javaCameraView 送到父 Activity，一旦权限被授予之后，javaCameraView 的 setCameraPermissionGranted 就会自动被调用。
+    @Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        Log.i(TAG, "getCameraViewList");
+        List<CameraBridgeViewBase> list = new ArrayList<>();
+        list.add(javaCameraView);
+        return list;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_get);
+        findView();
+    }
+
+    private void findView() {
+        javaCameraView = findViewById(R.id.javaCameraView);
+    }
+
+
+    @Override
+    public void onPause() {
+        Log.i(TAG, "onPause");
+        super.onPause();
+        if (javaCameraView != null) {
+            javaCameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        Log.i(TAG, "onResume");
+        super.onResume();
+        if (OpenCVLoader.initDebug()) {
+            Log.i(TAG, "initDebug true");
+            baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        } else {
+            Log.i(TAG, "initDebug false");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, baseLoaderCallback);
+        }
     }
 }
+
+
