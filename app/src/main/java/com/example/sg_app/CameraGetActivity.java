ab6ccdaf0b2b5b1bc5e8445c6e0c416407317c22
KagruaChen <2684607398@qq.com>
2023-02-08 16:57:42 +0800

    ImgReturn

--- app/src/main/java/com/example/sg_app/CameraGetActivity.java
+++ app/src/main/java/com/example/sg_app/CameraGetActivity.java
@@ -1,10 +1,10 @@
 package com.example.sg_app;
 
 
-import android.content.Intent;
+
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.KeyEvent;
 
@@ -16,15 +16,14 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.core.Size;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,54 +34,55 @@ public class CameraGetActivity extends CameraActivity {
     private static final String TAG = "OpencvCam";
 
     private JavaCamera2View javaCameraView;
-    private int cameraId = JavaCamera2View.CAMERA_ID_ANY;
+//    private int cameraId = JavaCamera2View.CAMERA_ID_ANY;
 
     private Socket socket;
-    private PrintWriter pw,ps;
-    private InputStream is;
-    private OutputStream os;
+    private OutputStreamWriter osw;
+    private BufferedOutputStream bos;
+    private BufferedReader br;
+    private BufferedInputStream bis;
 
     private Mat img;
-    private BufferedReader br;
-    private String recevied;
+    private Mat receiveImg;
+    private boolean exitFlag;
+
 
 
     private CameraBridgeViewBase.CvCameraViewListener2 cvCameraViewListener2 = new CameraBridgeViewBase.CvCameraViewListener2() {
         @Override
         public void onCameraViewStarted(int width, int height) {
-            Log.i(TAG, "onCameraViewStarted width=" + width + ", height=" + height);
+//            Log.i(TAG, "onCameraViewStarted width=" + width + ", height=" + height);
         }
 
         @Override
         public void onCameraViewStopped() {
-            Log.i(TAG, "onCameraViewStopped");
+//            Log.i(TAG, "onCameraViewStopped");
         }
 
         @Override
         public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
             img = inputFrame.rgba();
             Mat img0 =  img.clone();//复制图像矩阵用于处理
-            Mat dst = new  Mat();
-            br=new BufferedReader(new InputStreamReader(is));
-            Imgproc.resize(img0,dst, new Size(256, 256));//放缩图像降低传输的延时
-            Bitmap bitmap = Bitmap.createBitmap(dst.width(), dst.height(), Bitmap.Config.ARGB_8888);
-            Utils.matToBitmap(dst, bitmap,true);//添加透明度，转变Mat为Bitmap
+//            Imgproc.resize(img0,img0, new Size(24, 24));//放缩图像降低传输的延时
+            Bitmap bitmap = Bitmap.createBitmap(img0.width(), img0.height(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(img0, bitmap,true);//转变Mat为Bitmap
             ByteArrayOutputStream bout = new ByteArrayOutputStream();
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bout);
             byte[] imgBytes = bout.toByteArray();
-            String sender = Base64.encodeToString(imgBytes,Base64.DEFAULT);//Base64编码
-            String len_str = new String(String.format("%-16d", sender.length()).getBytes());
-            pw.write(len_str);//第一次发送编码的长度
-            pw.flush();
+//            String sender = Base64.encodeToString(imgBytes,Base64.DEFAULT);//Base64编码
+            String len_str = String.format("%-16d", imgBytes.length);
+
             try {
-                recevied = br.readLine();//由于读取每一行信息，故服务器发送的数据也应做处理
-                System.out.println(recevied);
+                osw.write(len_str);//第一次发送长度
+                osw.flush();
+//                System.out.println(imgBytes);
+                bos.write(imgBytes);//第二次发送图片
+                bos.flush();
             } catch (IOException e) {
-                System.out.println(e);
+                e.printStackTrace();
             }
-            ps.write(sender);//第二次发送编码信息
-            ps.flush();
-            return img;
+//            receiveImg = Imgcodecs.imdecode(new MatOfByte(imgBytes), Imgcodecs.IMREAD_COLOR);
+            return receiveImg;
         }
     };
 
@@ -92,7 +92,7 @@ public class CameraGetActivity extends CameraActivity {
 private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
-            Log.i(TAG, "onManagerConnected status=" + status + ", javaCameraView=" + javaCameraView);
+//            Log.i(TAG, "onManagerConnected status=" + status + ", javaCameraView=" + javaCameraView);
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS: {
                     if (javaCameraView != null) {
@@ -104,8 +104,7 @@ private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
                 }
                 break;
                 default:
-                    super.onManagerConnected(status);
-                    break;
+                    super.onManagerConnected(status);break;
             }
         }
     };
@@ -113,7 +112,7 @@ private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
     //复写父类的 getCameraViewList 方法，把 javaCameraView 送到父 Activity，一旦权限被授予之后，javaCameraView 的 setCameraPermissionGranted 就会自动被调用。
     @Override
     protected List<? extends CameraBridgeViewBase> getCameraViewList() {
-        Log.i(TAG, "getCameraViewList");
+//        Log.i(TAG, "getCameraViewList");
         List<CameraBridgeViewBase> list = new ArrayList<>();
         list.add(javaCameraView);
         return list;
@@ -124,14 +123,63 @@ private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_get);
         findView();
+        exitFlag = false;
         socket = ((MySocket)getApplication()).getSocket();
         try {
-            os = socket.getOutputStream();
-            pw = new PrintWriter(os);
-            ps = new PrintWriter(os);
-            is = socket.getInputStream();
+            osw = new OutputStreamWriter(socket.getOutputStream());
+            bos = new BufferedOutputStream(socket.getOutputStream());
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            bis = new BufferedInputStream(socket.getInputStream());
         }catch (IOException e) {
-            e.printStackTrace();}
+            e.printStackTrace();
+
+        }
+
+        Thread myThread=new Thread(){//创建子线程
+            @Override
+            public void run() {
+                byte[] imgLen_int = new byte[16];
+                byte[] array;
+                Bitmap returnBmp;
+                Integer leaveLen, imgLen;
+                int cnt = 1;
+                while (true) {
+                    if (exitFlag)
+                        break;
+                    try {
+                        while (bis.read(imgLen_int) == 0);
+                        imgLen = Integer.parseInt((new String(imgLen_int, 0, 16)).trim());
+                        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                        byte[] b = new byte[1024];
+                        int len;
+                        leaveLen = imgLen;
+                        while (leaveLen != 0) {
+                            if (leaveLen <= 1024) {
+                                b = new byte[leaveLen];
+                            }
+                            len = bis.read(b);
+                            bos.write(b, 0, len);
+                            leaveLen -= len;
+                        }
+                        array = bos.toByteArray();
+                        bos.close();
+                        returnBmp = BitmapFactory.decodeByteArray(array, 0, imgLen);
+                        Mat recImg = new Mat();
+                        System.out.println("接收照片"+cnt+"张");
+                        ++cnt;
+                        Utils.bitmapToMat(returnBmp, recImg);
+                        if (recImg != null)
+                            receiveImg = recImg;
+//                         reciveImg = Imgcodecs.imdecode(new MatOfByte(array), Imgcodecs.IMREAD_COLOR);
+                        Thread.sleep(1);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        };
+        myThread.start();//启动线程
         }
 
     private void findView() {
@@ -150,23 +198,38 @@ private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
 
     @Override
     public void onResume() {
-        Log.i(TAG, "onResume");
+//        Log.i(TAG, "onResume");
         super.onResume();
         if (OpenCVLoader.initDebug()) {
-            Log.i(TAG, "initDebug true");
+//            Log.i(TAG, "initDebug true");
             baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         } else {
-            Log.i(TAG, "initDebug false");
+//            Log.i(TAG, "initDebug false");
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, baseLoaderCallback);
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            exitFlag = true;
+            socket.close();
+            osw.close();
+            bos.close();
+            br.close();
+            bis.close();
+            CameraGetActivity.this.finish();
+            System.out.println("结束");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
-
-            Intent resultIntent=new Intent(CameraGetActivity.this, MainActivity.class);
             CameraGetActivity.this.finish();
-            startActivity(resultIntent);
         }
         return false;
     }
--- app/src/main/java/com/example/sg_app/MainActivity.java
+++ app/src/main/java/com/example/sg_app/MainActivity.java
@@ -6,38 +6,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button_1;
+    private EditText ip;
+    private EditText port;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Thread myThread=new Thread(){//创建子线程
-            @Override
-            public void run() {
-                try{
-                    Socket socket = new Socket("127.0.0.1",Integer.parseInt("5050"));
-                    ((MySocket)getApplication()).setSocket(socket);//初始化MySocket
-                    System.out.println("ok");
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-        };
-        myThread.start();//启动线程
 
+        ip = findViewById(R.id.et_1);
+        port = findViewById(R.id.et_2);
         button_1 = findViewById(R.id.openCamera);
         button_1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, CameraGetActivity.class);
-                MainActivity.this.startActivity(intent);//启动新的Intent
+
+                Thread myThread=new Thread(){//创建子线程
+                    @Override
+                    public void run() {
+                        try{
+                            Socket socket = new Socket(ip.getText().toString(),Integer.parseInt(port.getText().toString()));
+                            ((MySocket)getApplication()).setSocket(socket);//初始化MySocket
+//                            System.out.println("ok");
+
+                            Intent intent = new Intent();
+                            intent.setClass(MainActivity.this, CameraGetActivity.class);
+                            MainActivity.this.startActivity(intent);//启动新的Intent
+                        }catch (Exception e){
+                            e.printStackTrace();
+
+                        }
+
+                    }
+                };
+                myThread.start();//启动线程
+
+
 
             }
         });
